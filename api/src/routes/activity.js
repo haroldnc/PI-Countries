39f4a3c02b763ef42c5b2301f37588d810c01aff
Jack Navarro <harold.mth95@gmail.com>
2022-05-04 03:12:03 -0500

    Agregamos el paquete axios y las rutas

---
+++ api/src/routes/activity.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const { Tourist_activity } = require('../db');
+const router = Router();
+
+router.post('/', async (req, res) => {
+   const {
+      name,
+      difficulty,
+      duration,
+      season,
+      countriesId
+   } = req.body;
+
+   try {
+      const [activity,] = await Tourist_activity.findOrCreate({
+         where: {
+            name,
+            difficulty,
+            duration,
+            season
+         }
+      });
+
+      countriesId.forEach(async (countryId) => {
+         await activity.addCountries(countryId);
+      });
+
+      res.status(200).json({ message: 'The activity was sucessfully created' });
+   } catch (error) {
+      res.status(404).json({ error: error.message });
+   }
+});
+
+module.exports = router;
---
+++ api/src/routes/countries.js
@@ -0,0 +1,68 @@
+const { Router } = require('express');
+const { Op } = require('sequelize');
+const { Country, Tourist_activity } = require('../db');
+const axios = require('axios');
+const router = Router();
+const urlApi = 'https://restcountries.com/v3/all';
+
+router.get('/', async (req, res) => {
+   const { name } = req.query;
+   let data;
+
+   try {
+      if (name){
+         data = await Country.findAll({
+            where: {
+               name: {
+                  [Op.iLike]: `%${name}%`
+               }
+            }
+         });
+      } else {
+         data = await Country.findAll();
+
+         if (!data.length){
+            const result = await axios.get(urlApi);
+
+            result.data.forEach(country => {
+               data.push({
+                  id: country.cca3,
+                  name: country.name.common,
+                  flag: country.flags[0],
+                  continent: country.continents[0],
+                  capital: (country.capital && country.capital[0]) || '',
+                  subregion: country.subregion || country.region,
+                  area: country.area,
+                  population: country.population
+               });
+
+               Country.create(data[data.length-1]);
+            });
+         }
+      }
+
+      res.status(200).json(data);
+   } catch (error) {
+      res.status(404).json({ error: error.message });
+   }
+});
+
+router.get('/:idCountry', async (req, res) => {
+   const { idCountry } = req.params;
+   const { fields } = req.body;
+
+   try {
+      const result = await Country.findByPk(idCountry, {
+         attributes: fields,
+         include: 'activities'
+      });
+
+      if (!result) throw new Error('The country was not found');
+
+      res.status(200).json(result);
+   } catch (error) {
+      res.status(404).json({ error: error.message });
+   }
+});
+
+module.exports = router;
